d5c43e5f9fdd0907d722e3416867fc32f2000005
agix <florian.gaultier@gmail.com>
2014-12-09 19:12:54 +0100

    allow 50 thread

---
+++ ids_bypass.rb
@@ -0,0 +1,103 @@
+#!/usr/bin/env ruby
+# -*- coding: binary -*-
+require 'packetfu'
+require 'socket'
+require 'netaddr'
+
+iface = ARGV[0] || "eth0"
+ahosts = ARGV[1] || "127.0.0.1"
+aports = ARGV[2] || "8080"
+
+def connect(host, port, timeout = 5)
+
+  # Convert the passed host into structures the non-blocking calls
+  # can deal with
+  addr = Socket.getaddrinfo(host, nil)
+  sockaddr = Socket.pack_sockaddr_in(port, addr[0][3])
+
+  Socket.new(Socket.const_get(addr[0][0]), Socket::SOCK_STREAM, 0).tap do |socket|
+    socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+
+    begin
+      # Initiate the socket connection in the background. If it doesn't fail
+      # immediatelyit will raise an IO::WaitWritable (Errno::EINPROGRESS)
+      # indicating the connection is in progress.
+      socket.connect_nonblock(sockaddr)
+
+    rescue IO::WaitWritable
+      # IO.select will block until the socket is writable or the timeout
+      # is exceeded - whichever comes first.
+      if IO.select(nil, [socket], nil, timeout)
+        begin
+          # Verify there is now a good connection
+          socket.connect(sockaddr)
+        rescue Errno::EISCONN
+          # Good news everybody, the socket is connected!
+        rescue
+          # An unexpected exception was raised - the connection is no good.
+          socket.close
+          raise
+        end
+      else
+        # IO.select returns nil when the socket is not ready before timeout
+        # seconds have elapsed
+        socket.close
+        raise "Connection timeout"
+      end
+    end
+  end
+end
+
+def analyse(iface, hosts, ports)
+  cap = PacketFu::Capture.new(:iface => iface, :start => true, :filter => "tcp")
+  cap.stream.each do |pkt|
+    packet = PacketFu::Packet.parse pkt
+    if hosts.index(packet.ip_saddr) && ports.index(packet.tcp_src) && packet.tcp_flags.fin == 1
+      print "#{packet.ip_saddr}:#{packet.tcp_src} is open\n"
+    end
+  end
+end
+
+hosts = NetAddr::CIDR.create(ahosts)
+ports = []
+
+sports = aports.split(',')
+
+for tport in sports
+    if tport.index('-')
+      aports = (tport.split('-')[0].to_i..tport.split('-')[1].to_i+1).to_a
+      ports += aports
+    else
+      ports << tport.to_i
+    end
+end
+ports = ports.uniq.sort
+
+sn = Thread.new {analyse(iface, hosts.enumerate, ports)}
+
+threads = []
+sockets = []
+host_t = []
+port_t = []
+
+sleep(1)
+for host in hosts.enumerate
+  for port in ports
+    begin
+      if threads.length<50
+        threads << Thread.new(host,port) {
+          |h,p|
+          s = connect(h, p, 1)
+          s.close()
+        }
+      else
+        s = connect(host, port, 1)
+        s.close()
+      end
+    rescue Exception => e
+      b = ""
+    end
+  end
+end
+
+sleep(5)
